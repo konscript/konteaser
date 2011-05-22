9f4d0ab9631093064c558b25c92351cfb754a3c2
laander <la@laander.com>
2011-05-22 18:38:03 +0200

    added chartbeat tracking code

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 	<head>
+
+		<script type="text/javascript">var _sf_startpt=(new Date()).getTime()</script>
 	
 		<title>Konscript | An Agency of Digital Natives</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -79,6 +81,26 @@
 			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 			})();
+		
+		</script>
+
+		<script type="text/javascript">
+		var _sf_async_config={uid:24249,domain:"konscript.com"};
+		(function(){
+		  function loadChartbeat() {
+		    window._sf_endpt=(new Date()).getTime();
+		    var e = document.createElement('script');
+		    e.setAttribute('language', 'javascript');
+		    e.setAttribute('type', 'text/javascript');
+		    e.setAttribute('src',
+		       (("https:" == document.location.protocol) ? "https://a248.e.akamai.net/chartbeat.download.akamai.com/102508/" : "http://static.chartbeat.com/") +
+		       "js/chartbeat.js");
+		    document.body.appendChild(e);
+		  }
+		  var oldonload = window.onload;
+		  window.onload = (typeof window.onload != 'function') ?
+		     loadChartbeat : function() { oldonload(); loadChartbeat(); };
+		})();
 		
 		</script>
 
