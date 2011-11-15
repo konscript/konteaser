e0d4b4a803de6c5d444d980fafdfaa9a17d7e5c1
laander <la@laander.com>
2011-11-15 15:52:37 +0100

    fiddled around with urls in compass

--- config.rb
+++ config.rb
@@ -4,7 +4,7 @@ css_dir         = "css"
 sass_dir        = "css"
 images_dir      = "img"
 javascripts_dir = "js" 
-output_style = :compressed
+output_style    = :expanded
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
